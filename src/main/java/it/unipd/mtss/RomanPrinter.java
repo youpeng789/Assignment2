a37a576da6aed0f7dc5b25f4fc42a7985aa1f743
Álvaro Martín Bacas <amb00093@red.ujaen.es>
2024-05-14 21:17:10 +0200

    close #6

---
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -0,0 +1,15 @@
+////////////////////////////////////////////////////////////////////
+// Youpeng Liu 2076428
+// Álvaro Martín 2099592
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+public class RomanPrinter {
+    public static String print(int num){
+        return printAsciiArt(IntegerToRoman.convert(num));
+    }
+    private static String printAsciiArt(String romanNumber){
+        //TODO
+        return null;
+    }
+}
